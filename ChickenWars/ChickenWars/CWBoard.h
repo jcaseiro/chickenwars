7d9fdd0bb6dedd3d5a269321b5e6ecd9f579b6b8
iOS Course <josemartins@Joses-MacBook-Pro.local>
2014-10-08 19:33:38 +0100

    Initial commit

---
+++ ChickenWars/ChickenWars/CWBoard.h
@@ -0,0 +1,13 @@
+//
+//  CWBoard.h
+//  ChickenWars
+//
+//  Created by iOS Course on 08/10/14.
+//  Copyright (c) 2014 Wit Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWBoard : NSObject
+
+@end
